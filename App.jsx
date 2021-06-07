3c1066514fe2376ecdf566e03886ee3cf0978986
hemalatha02 <76465555+hemalatha02@users.noreply.github.com>
2021-06-07 12:35:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Node from "./Node";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Node />
+    </div>
+  );
+}
+
+export default App;
---
+++ Node.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Node() {
+  return (
+    <div className="Node">
+      <h1> Javascript and React.js</h1>
+      <p> This is a nice Bootcamp</p>
+    </div>
+  );
+}
+
+export default Node;
